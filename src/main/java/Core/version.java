0ce67cc318a03081b7c51bce184b72934b75bf4d
uis246 <uis9936@gmail.com>
2018-02-05 23:26:25 +0300

    Just

--- src/main/java/Core/version.java
+++ src/main/java/Core/version.java
@@ -1,6 +1,6 @@
 package Core;
 public class version {
-	public static final String CoreVersion="0.2.3";
+	public static final String CoreVersion="0.2.4";
 	public static final byte CoreBuild=(byte) 0x00;
 	public static String getCoreVersion(){
 		return CoreVersion;
